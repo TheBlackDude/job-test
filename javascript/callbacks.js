46d8e4388d03f7c6fadc7e58a751205705ce98eb
Ousmane Diallo <mudu.jalloh@gn.ehealthafrica.org>
2017-09-04 15:27:42 +0100

    fix: refactored callbacks to Promises.

--- javascript/callbacks.js
+++ javascript/callbacks.js
@@ -2,22 +2,31 @@ const books = require('./books')
 
 const print = result => console.log('Result:', result)
 
-const filterBooksWithTitleStartingWithA = (sortedBooks, callback) => {
-  let filteredBooks
-  /* filter logic here */
-  return callback(filteredBooks)
+const filterBooksWithTitleStartingWithA = (sortedBook) => {
+  return new Promise((resolve, reject) => {
+    resolve(sortedBook.filter(value => value.title.toLowerCase().startsWith('a')))
+  })
 }
 
-const sortBooksAlphabetically = (books, callback) => {
-  let sortedBooks
-  /* sorting logic books */
-  return callback(sortedBooks, print)
+const sortBooksAlphabetically = (booksData) => {
+  return new Promise((resolve, reject) => {
+    resolve(booksData.sort((a, b) => a.title > b.title))
+  })
 }
 
-const getBooksAsync = (callback) => {
-  setTimeout(() => {
-    callback(books, filterBooksWithTitleStartingWithA)
-  }, 2000)
+const getBooksAsync = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(books.map(value => value))
+    }, 2000)
+  })
 }
 
-getBooksAsync(sortBooksAlphabetically)
+getBooksAsync()
+  .then(data => {
+    return sortBooksAlphabetically(data)
+  })
+  .then(data => {
+    return filterBooksWithTitleStartingWithA(data)
+  })
+  .then(data => print(data))
