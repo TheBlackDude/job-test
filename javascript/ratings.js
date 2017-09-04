06230218c27acae2bbfc77ee71856d6c67e4280d
Ousmane Diallo <mudu.jalloh@gn.ehealthafrica.org>
2017-09-04 02:30:33 +0100

    fix: change comments.

--- javascript/ratings.js
+++ javascript/ratings.js
@@ -1,6 +1,9 @@
 module.exports = (books) => {
   const ratings = {}
-  /* sort books by ratings */
+  /*
+   * sort books by ratings (here i don't understand whether to make a copy of the books or sort the received ones)
+   * because sort will not return a copy. but i assume its to sort the original 
+   */
   books.sort((a, b) => a.ratings < b.ratings)
 
   ratings.getTopBooks = (numberOfBooks) => {
