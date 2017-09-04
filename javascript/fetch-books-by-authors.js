678fe97b8c26730388a87225e03abc74f925e325
Ousmane Diallo <mudu.jalloh@gn.ehealthafrica.org>
2017-09-04 01:32:07 +0100

    fix: change getBooksByAuthor function.

--- javascript/fetch-books-by-authors.js
+++ javascript/fetch-books-by-authors.js
@@ -2,6 +2,6 @@ const books = require('./books')
 
 /* return books by author */
 const getBooksByAuthor = (author) => {
-  const booksByAuthor =  books.filter(value => value.author === author)
-  return booksByAuthor.map(value => value.title)
+  return books.filter(value => value.author === author)
+      .map(value => value.title)
 }
